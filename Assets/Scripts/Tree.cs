1b02030c93603caa7acfd811ffc0534178d3fe56
SaulHarwin <15swharwin@uckfield.college>
2020-12-22 15:02:15 +0000

    Library Changes

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class Tree : MonoBehaviour {
 
 	public float radius = 1;
-	public Vector3 regionSize = Vector3.one;
+	public Vector2 regionSize = Vector2.one;
 	public int rejectionSamples = 30;
-	public float displayRadius = 1;
+	public float displayRadius =1;
 
-	List<Vector3> points;
+	List<Vector2> points;
 
 	void OnValidate() {
-		points = TreeGeneration.GeneratePoints(radius, regionSize, rejectionSamples);
+		points = PoissonDiscSampling.GeneratePoints(radius, regionSize, rejectionSamples);
 	}
 
 	void OnDrawGizmos() {
 		Gizmos.DrawWireCube(regionSize/2,regionSize);
 		if (points != null) {
-			foreach (Vector3 point in points) {
+			foreach (Vector2 point in points) {
 				Gizmos.DrawSphere(point, displayRadius);
 			}
 		}
